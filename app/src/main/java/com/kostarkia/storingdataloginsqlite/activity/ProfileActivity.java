373a1adde9960439b7dca59dfdcbd132bbdee827
Ferhat Bolat <76790056+Kostarkia@users.noreply.github.com>
2023-03-06 09:05:33 +0300

    updated

--- app/src/main/java/com/kostarkia/storingdataloginsqlite/activity/ProfileActivity.java
+++ app/src/main/java/com/kostarkia/storingdataloginsqlite/activity/ProfileActivity.java
@@ -1,6 +1,9 @@
 package com.kostarkia.storingdataloginsqlite.activity;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,10 +11,20 @@ import com.kostarkia.storingdataloginsqlite.R;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        TextView textUsername = findViewById(R.id.textUsername);
+
+        SharedPreferences preferences = this.getSharedPreferences("com.kostarkia.storingdataloginsqlite", MODE_PRIVATE);
+
+        String getUser = preferences.getString("userName", null);
+
+        textUsername.setText(getUser);
+
     }
 
 
